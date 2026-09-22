ff8e2836de55d1d9fac397da68e845a7ff87d57c
Darya Drozhzha <darya.drozhzha@gmail.com>
2026-09-22 09:00:18 +0000

    Add SQL exercises: query24

---
+++ SQL exercises/query24.sql
@@ -0,0 +1,13 @@
+SELECT model
+FROM (SELECT model, price FROM PC
+      UNION
+      SELECT model, price FROM Laptop
+      UNION
+      SELECT model, price FROM Printer) cte1
+WHERE price = (SELECT MAX(price)
+               FROM (SELECT price FROM PC
+                     UNION
+                     SELECT price FROM Laptop
+                     UNION
+                     SELECT price FROM Printer) cte2
+              )
